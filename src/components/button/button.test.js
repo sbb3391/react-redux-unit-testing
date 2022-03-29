cfa177270699a19332231a3ca51af3c6dcae7f62
Sam Bequette <acmn2zz@mmm.com>
2022-03-29 11:32:17 -0400

    adds first test simulating click event on button component

--- src/components/button/button.test.js
+++ src/components/button/button.test.js
@@ -20,12 +20,11 @@ describe('Button Component', () => {
     
     describe('It renders an HTML Button', () => {
         let wrapper;
+        let mockFunc = jest.fn();
         beforeEach(() => {
             const props = {
                 buttonText: 'Example Text',
-                emitEvent: () => {
-
-                }
+                emitEvent: mockFunc
             }
             wrapper = shallow(<Button {...props} />)
         })
@@ -35,6 +34,15 @@ describe('Button Component', () => {
             const button = findByTestAttr(wrapper, 'buttonComponent');
 
             expect(button.length).toBe(1);
+        });
+
+        it('Should run a callback on clicking the button', () => {
+            // eslint-disable-next-line testing-library/await-async-query
+            const button = findByTestAttr(wrapper, 'buttonComponent');
+            button.simulate('click');
+            const callback = mockFunc.mock.calls.length;
+
+            expect(callback).toBe(1);
         })
     })
 })
